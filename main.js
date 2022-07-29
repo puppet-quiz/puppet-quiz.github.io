671015babe0dd3b68ec8923120bd3a9392b8aee9
puppet-quiz <110218516+puppet-quiz@users.noreply.github.com>
2022-07-29 23:04:38 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-    $(document).ready(function () {
-        $('#search').suggestionBox({
-            filter: true,
-            widthAdjustment: -8,
-            leftOffset: 4,
-            topOffset: 0,
-        }).loadSuggestions('/data.json');
-    });
