5d390504ba4ac0d168252857fbce16b7329f496d
puppet-quiz <110218516+puppet-quiz@users.noreply.github.com>
2022-07-31 01:32:39 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ document.getElementById('search_term').addEventListener('keyup',function(){
 		var regex = new RegExp(searchField, 'i');
 		var output = '';
 		output += '<tr class="result"><td style="text-align:center; background-color:#52525f;"><p>퀴즈 내용</p></td><td style="text-align:center; background-color:#005f6b;"><p>정답</p></td></tr>'
-		$.getJSON('quiz/data.json', function (data) {
+		$.getJSON("https://opensheet.elk.sh/1iuVShj94nRbP7XMBbPX1gdY5UhxlBGqAhbY9P-qU6wg/mq", function (data) {
 			$.each(data, function (key, val) {
 				if ((val.quiz.search(regex) != -1) || (val.answer.search(regex) != -1)) {
 					output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p></td><td class="정답"><p>' + val.answer + '</p></td></tr>';
