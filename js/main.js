d7d3dc8f44a9cb9df88585d187139f681bb26fe4
puppet-quiz <110218516+puppet-quiz@users.noreply.github.com>
2022-07-31 18:36:45 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,8 @@ document.getElementById('search_term').addEventListener('keyup',function(){
 		$.getJSON("https://opensheet.elk.sh/1iuVShj94nRbP7XMBbPX1gdY5UhxlBGqAhbY9P-qU6wg/mq", function (data) {
 			$.each(data, function (key, val) {
 				if ((val.quiz.search(regex) != -1) || (val.answer.search(regex) != -1)) {
-					output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p></td><td class="정답"><p>' + val.answer + '</p></td></tr>';
+//					output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p></td><td class="정답"><p>' + val.answer + '</p></td></tr>';
+					output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p><br></td><td class="정답"><p>' + val.answer + '</p></td></tr>';
 				}
 			});
 		$('#search_results').html(output);
