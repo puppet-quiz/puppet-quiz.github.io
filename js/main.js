385dbc540b5d60d82a77f067ddff1fc4c0e59198
puppet-quiz <110218516+puppet-quiz@users.noreply.github.com>
2022-08-06 01:35:15 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,33 +18,7 @@ document.getElementById('search_term').addEventListener('keyup',function(){
 //		output += '<tr class="result"><td style="text-align:center; background-color:#52525f;"><p>퀴즈 내용</p></td><td style="text-align:center; background-color:#005f6b;"><p>정답</p></td></tr>'
 		$.getJSON("https://opensheet.elk.sh/1iuVShj94nRbP7XMBbPX1gdY5UhxlBGqAhbY9P-qU6wg/mq", function (data) {
 		
-			//현재 data 상태
-				//data = ["김치 볶음밥","신라면","진라면","라볶이","팥빙수","너구리","삼양라면","안성탕면","불닭볶음면","짜왕","라면사리"];	
-		//이부분이 초성 검색이 가능하게 만들어 주는 부분
-
 			
-			let source = $.map(data, function(val.quiz) { //json[i] 번째 에 있는게 item 임.
-					chosung = "";
-					//Hangul.d(val.quiz, true) 을 하게 되면 item이 분해가 되어서 
-					//["ㄱ", "ㅣ", "ㅁ"],["ㅊ", "ㅣ"],[" "],["ㅂ", "ㅗ", "ㄲ"],["ㅇ", "ㅡ", "ㅁ"],["ㅂ", "ㅏ", "ㅂ"]
-					//으로 나오는데 이중 0번째 인덱스만 가지고 오면 초성이다.
-					
-					full = Hangul.disassemble(val.quiz).join("").replace(/ /gi, "") ;	//공백제거된 ㄱㅣㅁㅊㅣㅂㅗㄲㅇㅡㅁㅂㅏㅂ
-					
-					Hangul.d(val.quiz, true).forEach(function(strquiz, index) {
-						
-						if(strquiz[0] != " "){	//띄어 쓰기가 아니면
-							chosung += strquiz[0];//초성 추가
-							
-						}
-					});
-				return {
-						label : chosung + "|" + (val.quiz).replace(/ /gi, "") +"|" + full, //실제 검색어랑 비교 대상 ㄱㅊㅂㅇㅂ|김치볶음밥|ㄱㅣㅁㅊㅣㅂㅗㄲㅇㅡㅁㅂㅏㅂ 이 저장된다.
-						value : val.quiz, //김치 볶음밥
-						chosung : chosung,	//ㄱㅊㅂㅇㅂ,
-						full : full
-					}
-				});
 					
 		
 			
