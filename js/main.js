82c15706f6a4c374279050ec747d0f74de25bc01
puppet-quiz <110218516+puppet-quiz@users.noreply.github.com>
2022-08-07 16:20:26 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const Hangul = require('hangul-js');
+// const Hangul = require('hangul-js');
+
 var back_color="#000d03";
 var base_color="#fdf6e3";
 var ans_color="ffd700";
@@ -15,28 +16,23 @@ document.getElementById('search_term').addEventListener('keyup',function(){
 	if(searchField == ''){}
 	else{
 		var regex = new RegExp(searchField, 'i');
-	var regex1 = Hangul.disassemble(regex).join(""); // ㄺ=>ㄹㄱ
-
 		var output = '';
 //		output += '<tr class="result"><td style="text-align:center; background-color:#52525f;"><p>퀴즈 내용</p></td><td style="text-align:center; background-color:#005f6b;"><p>정답</p></td></tr>'
 		$.getJSON("https://opensheet.elk.sh/1iuVShj94nRbP7XMBbPX1gdY5UhxlBGqAhbY9P-qU6wg/mq", function (data) {
 		
-
 			
-		// object 에 초성필드 추가 {name:"홍길동", diassembled:"ㅎㄱㄷ"}        
+					
+		
+			
+			
+		
 			$.each(data, function (key, val) {
-				var dis = Hangul.disassemble(val.quiz, true);
-				var cho = dis.reduce(function (prev, elem) {
-				elem = elem[0] ? elem[0] : elem;
-				return prev + elem;
-				}, "");
-			val.diassembled = cho;
-            	// 문자열 검색 || 초성검색
-            if ((val.quiz.search(regex) != -1) || (val.diassembled.search(regex1) != -1) /*|| (val.answer.search(regex) != -1)*/) {
-                //output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p></td><td class="정답"><p>' + val.answer + '</p></td></tr>';
-                  output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p><p style="color:ffd700">' + val.answer + '</p></td></tr>';
-                                }
-                            });				
+//				Hangul.disassemble(val.quiz, true);
+				if ((val.quiz.search(regex) != -1) /*|| (val.answer.search(regex) != -1)*/) {
+//					output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p></td><td class="정답"><p>' + val.answer + '</p></td></tr>';
+					output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p><p style="color:ffd700">' + val.answer + '</p></td></tr>';
+				}
+			});
 		$('#search_results').html(output);
 		});
 	}
