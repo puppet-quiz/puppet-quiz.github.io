e5225affd2f5e09969954077497c2bde42fd89fb
puppet-quiz <110218516+puppet-quiz@users.noreply.github.com>
2022-08-12 01:36:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,14 +31,14 @@ document.getElementById('search_term').addEventListener('keyup',function(){
             val.diassembled = cho;
 			
 		
-//            var search = this.value;
-//            var search1 = Hangul.disassemble(search).join("");  // ㄺ=>ㄹㄱ
+  //          var search = this.value;
+            var search1 = Hangul.disassemble(regex).join("");  // ㄺ=>ㄹㄱ
 
 
     			
 			
             	// 문자열 검색 || 초성검색
-            if ((val.quiz.includes(regex) != -1) || (val.diassembled.includes(regex) != -1)) {           
+            if ((val.quiz.includes(regex) != -1) || (val.diassembled.includes(search1) != -1)) {           
 		                              
    
                   output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p><p style="color:ffd700">' + val.answer + '</p></td></tr>';
