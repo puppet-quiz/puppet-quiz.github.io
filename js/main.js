f5784c2d2bafd852b302bb1dd19bc13eb7adbd4c
puppet-quiz <110218516+puppet-quiz@users.noreply.github.com>
2022-08-16 23:51:02 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,18 +8,7 @@ function applycolor(b_color,b2_color,a_color){
 
 }
 
-document.getElementById('search_term').addEventListener('keyup',function(){
-	$('.result').removeClass('result');
-	var searchField = $('#search_term').val();
-	if(searchField == '' || searchField == ' '){}
-	else{
-		$.getJSON("https://opensheet.elk.sh/1iuVShj94nRbP7XMBbPX1gdY5UhxlBGqAhbY9P-qU6wg/mq", function (data) {
-
-	
-		var output = '';	
-		var regex = new RegExp(searchField, 'i');	
-			
-		$.each(data, function (key, val) {	
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('7.8(\'3\').9(\'a\',1(){$(\'.4\').b(\'4\');2 0=$(\'#3\').5();c(0==\'\'||0==\' \'){}d{$.e("f://g.h.j/k-l/m",1(6){2 n=\'\';2 o=p q(0,\'i\');$.r(6,1(s,5){',29,29,'searchField|function|var|search_term|result|val|data|document|getElementById|addEventListener|keyup|removeClass|if|else|getJSON|https|opensheet|elk||sh|1iuVShj94nRbP7XMBbPX1gdY5UhxlBGqAhbY9P|qU6wg|mq|output|regex|new|RegExp|each|key'.split('|'),0,{}))
 
 /*	// object 에 초성필드 추가 {name:"홍길동", diassembled:"ㅎㄱㄷ"}
         
@@ -37,17 +26,8 @@ document.getElementById('search_term').addEventListener('keyup',function(){
 
     			
 			
-            	// 문자열 검색 || 초성검색*/
-            if ((val.quiz.search(regex) != -1) /*|| (val.diassembled.search(regex) != -1)*/) {           
-		                              
-   
-                  output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p><p style="color:ffd700">' + val.answer + '</p></td></tr>';
-                                }
-                            });				
-		$('#search_results').html(output);
-		});
-	}
-});
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('8((2.3.9(a)!=-1)){4+=\'<5 6="b"><7 6="퀴즈"><0>\'+2.3+\'</0><0 c="d:e">\'+2.f+\'</0></7></5>\'}});$(\'#g\').h(4)})}});',18,18,'p||val|quiz|output|tr|class|td|if|search|regex|result|style|color|ffd700|answer|search_results|html'.split('|'),0,{}))
 
 
 applycolor(back_color,base_color,ans_color);
