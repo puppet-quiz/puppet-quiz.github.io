cccfeec3823e12b3d5f9e35b923b889f2c2cb144
puppet-quiz <110218516+puppet-quiz@users.noreply.github.com>
2022-08-17 01:07:42 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,15 +8,14 @@ function applycolor(b_color,b2_color,a_color){
 
 }
 
-eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0.1(\'2\').3(\'4\',5()',6,6,'document|getElementById|search_term|addEventListener|keyup|function'.split('|'),0,{}))
-	{
-	eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(\'.0\').1(\'0\')',2,2,'result|removeClass'.split('|'),0,{}));
-	eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0 1=$(\'#2\').3()',4,4,'var|searchField|search_term|val'.split('|'),0,{}));
-	eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1(0==\'\'||0==\' \')',2,2,'searchField|if'.split('|'),0,{}))
-	{}
+document.getElementById('search_term').addEventListener('keyup',function(){
+	$('.result').removeClass('result');
+	var searchField = $('#search_term').val();
+	if(searchField == '' || searchField == ' '){}
 	else{
-		eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$.0("1://2.3.4/5-6/7",8(9)',10,10,'getJSON|https|opensheet|elk|sh|1iuVShj94nRbP7XMBbPX1gdY5UhxlBGqAhbY9P|qU6wg|mq|function|data'.split('|'),0,{})) 
-		{	
+		$.getJSON("https://opensheet.elk.sh/1iuVShj94nRbP7XMBbPX1gdY5UhxlBGqAhbY9P-qU6wg/mq", function (data) {
+
+	
 		var output = '';	
 		var regex = new RegExp(searchField, 'i');	
 			
@@ -36,19 +35,15 @@ eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k
   //          var search1 = Hangul.disassemble(regex).join("");  // ㄺ=>ㄹㄱ
 */
 
-            eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0((2.3.4(5)!=-1))',6,6,'if||val|quiz|search|regex'.split('|'),0,{})) {           
+            if ((val.quiz.search(regex) != -1) /*|| (val.diassembled.search(regex) != -1)*/) {           
 		                              
    
-                  eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5+=\'<1 2="6"><3 2="퀴즈"><0>\'+4.7+\'</0><0 8="9:a">\'+4.b+\'</0></3></1>\'',12,12,'p|tr|class|td|val|output|result|quiz|style|color|ffd700|answer'.split('|'),0,{}));
+                  output += '<tr class="result"><td class="퀴즈"><p>' + val.quiz + '</p><p style="color:ffd700">' + val.answer + '</p></td></tr>';
                                 }
                             });				
 		$('#search_results').html(output);
 		});
 	}
-});    			
-			
-
-
-
+});			
 
 applycolor(back_color,base_color,ans_color);
